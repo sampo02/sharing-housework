081995eb8429833cb78462ab0fb7d425f59696e9
sampo02 <m.nishitsuji@gmail.com>
2021-07-21 14:01:15 +0900

    :sparkles: Show sign in with google (#3)

--- lib/features/housework/data/models/task_model.dart
+++ lib/features/housework/data/models/task_model.dart
@@ -1,19 +1,13 @@
 import 'package:sharing_housework/features/housework/domain/entities/task.dart';
 
 class TaskModel extends Task {
-  TaskModel({
-    required String name
-  }) : super(name: name);
+  TaskModel({required String name}) : super(name: name);
 
   factory TaskModel.fromJson(Map<String, dynamic> json) {
-    return TaskModel(
-      name: json['name']
-    );
+    return TaskModel(name: json['name']);
   }
 
   Map<String, dynamic> toJson() {
-    return {
-      'name': name
-    };
+    return {'name': name};
   }
 }
--- lib/features/housework/data/repositories/task_repository_impl.dart
+++ lib/features/housework/data/repositories/task_repository_impl.dart
@@ -1,7 +1,6 @@
 import 'package:sharing_housework/features/housework/domain/repositories/task_repository.dart';
 
 class TaskRepositoryImpl implements TaskRepository {
-
   @override
   void store() {
     // TODO: implement store
