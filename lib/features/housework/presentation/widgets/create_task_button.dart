bbc307298d75dfa72b05c15c8ed6ed452b546f5a
sampo02 <m.nishitsuji@gmail.com>
2021-07-22 22:50:53 +0900

    :sparkles: Add task model at presentation

--- lib/features/housework/presentation/widgets/create_task_button.dart
+++ lib/features/housework/presentation/widgets/create_task_button.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:sharing_housework/features/housework/domain/entities/task.dart';
-import 'package:sharing_housework/features/housework/domain/usecases/create_task_usecase.dart';
+import 'package:sharing_housework/features/housework/presentation/models/task_model.dart';
 import 'package:sharing_housework/injection.dart';
 
 class CreateTaskButton extends StatelessWidget {
   const CreateTaskButton({Key? key}) : super(key: key);
 
   Future<void> _createTask() async {
-    final createTaskUsecase = instance<CreateTaskUsecase>();
-    await createTaskUsecase.call(Task(name: 'New Task'));
+    final taskModel = instance<TaskModel>();
+    await taskModel.create('New Task');
   }
 
   @override
